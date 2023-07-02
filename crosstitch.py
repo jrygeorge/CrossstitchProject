2fe7fa1779d7eeee05502725e6be818a63b6f709
JerryGeorge101 <jerry99george@gmail.com>
2023-07-02 02:13:52 +0100

    Removed complicated numpy-slicing pixelating bit. Replaced it with simple pillow resize. Checked with time, its faster too. Inluded small tests to see if new pixels are pure. Will remove these tests later.

--- crosstitch.py
+++ crosstitch.py
@@ -4,32 +4,38 @@ from PIL import Image
 flag=True
 while flag:
     try:
-        bsize = int(input("Enter block size : "))
+        bsize = int(str.strip(input("Enter block size : ")))
     except:
         print("Invalid Input")
     else:
-        flag = not flag
+        flag = not flag #break
+
 original = Image.open("Dog.jpg") #Reading file
 w, h = original.size
 w=(w//bsize)*bsize
 h=(h//bsize)*bsize
 original=original.crop((0,0,w,h))
 original_info = np.asarray(original) #Array-ifiying
-new_info=original_info.copy()
 
 def main():
-    # new_info = np.fromfunction(average_colour, original_info.shape, dtype=int)
-    # this way isnt working for some reason, ive deleted the function its calling
-    # new_image = Image.fromarray(new_info, mode="RGB")
-    jam=list(range(0,h,bsize))
-    for i in range(0,h,bsize):
-        for j in range(0,w,bsize):
-            #print("{} {}".format(i,j))
-            new_info[i:None if i+bsize>h else i+bsize,j:None if j+bsize>w else j+bsize]=(
-            new_info[i:None if i+bsize>h else i+bsize,j:None if j+bsize>w else j+bsize].mean(axis=(0,1)))
-    
-    bim=Image.fromarray(new_info,mode="RGB")
-    bim.show()
+    new_pic=original.copy().resize((int(w/bsize),int(h/bsize)),4).resize((w,h),4)
+    new_pic.show()
+    new_info=np.asarray(new_pic)
+    #below 5 lines tests if the new format is producing pure squares, not squares with other colours
+    #reshaping to single line of pixels with 3 unit RGB array in each cell
+    strim=new_info.copy().reshape((w*h,3))
+
+    #display 500th pixel
+    print(strim[500])
+    #display 500th pixel directly from image
+    #result : theyre the same :)
+    print(new_pic.getpixel((500,1)))
+
+    #finding all unique pixels/colours and displaying the number
+    #if there are more colours than boxes (manual count), somethings wrong
+    #result : more boxes than colours :)
+    print(np.unique(strim,axis=0).shape)
+    print(strim.shape)
 
 if __name__ == "__main__":
     main()
