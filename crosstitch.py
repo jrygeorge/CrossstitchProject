998a1643e889cd9f5ceb3a58eb1ba58ae76c81d6
JerryGeorge101 <jerry99george@gmail.com>
2023-07-03 23:48:34 +0100

    k-means part, done !

--- crosstitch.py
+++ crosstitch.py
@@ -1,6 +1,7 @@
 #yes i know it says crosstitch, not cross stitch
 from PIL import Image
 import numpy as np
+from sklearn.cluster import KMeans
 flag=True
 block_size=0
 while flag:
@@ -14,7 +15,7 @@ while flag:
     else:
         flag = not flag #break
 
-original = Image.open("bob.png") #Reading file
+original = Image.open("Dog.jpg") #Reading file
 w, h = original.size
 # math to calculate block size, max to prevent it from being zero
 block_size= int(max( w / (ct * pic_width), 1 ))
@@ -27,11 +28,15 @@ def main():
     new_pic.show()
     # .png has 4 properties per pixel R,G,B,Transparency, we're getting rid of the last one
     # .delete implicitly converts to array and deletes Transparency
-    foo=np.delete(new_pic,3,2) if len(np.asarray(new_pic))>3 else new_pic
-    print(foo[200,200])
+    foo = np.delete(new_pic,3,2) if np.asarray(new_pic).shape[2]>3 else new_pic
     uncluster=np.reshape(foo,(w*h,3)) #long array with 3 properties each
-    print(uncluster[200])
-
+    model = KMeans(n_clusters=15,n_init=8)
+    cs_model=model.fit(uncluster)
+    baz=cs_model.cluster_centers_
+    gorb = np.array([baz[i].astype(int) for i in cs_model.labels_])
+    lembas = np.reshape(gorb,(w,h,3))
+    dot=Image.fromarray(lembas.astype(np.uint8), mode="RGB")
+    dot.show()
 
 if __name__ == "__main__":
     main()
